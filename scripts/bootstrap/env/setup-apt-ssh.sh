de0b4256a6eb35142d53702a76e2f60a0f8d0701
xzxz <46389288+J-XZ@users.noreply.github.com>
2024-04-15 14:34:22 +0800

    1

--- scripts/bootstrap/env/setup-apt-ssh.sh
+++ scripts/bootstrap/env/setup-apt-ssh.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 # cloudlab节点列表
-node_list=("clnode277" "clnode283")
+node_list=("clnode277")
+# node_list=("clnode277" "clnode283")
 
 node_list_size=${#node_list[@]}
 
 END=$node_list_size
 
-REMOTE_HOME="/proj/bg-PG0"
-setup_script="$REMOTE_HOME/haoyu/scripts/env"
-limit_dir="$REMOTE_HOME/haoyu/scripts"
+REMOTE_HOME="/users/ruixuan/NovaLSM"
+setup_script="$REMOTE_HOME/scripts/env"
+limit_dir="$REMOTE_HOME/scripts"
 
-LOCAL_HOME="/Users/haoyuh/Documents/PhdUSC/dblab/nova/NovaLSM/scripts/bootstrap"
+LOCAL_HOME="/Users/jiangxuzhen/Downloads/e-lsmtree/NovaLSM/scripts/bootstrap"
 
-host="Nova.bg-PG0.apt.emulab.net"
+# ssh ruixuan@clnode277.clemson.cloudlab.us
+
+# host="Nova.bg-PG0.apt.emulab.net"
+host="clemson.cloudlab.us"
 # host="Nova.bg-PG0.utah.cloudlab.us"
 
 # scp -r $LOCAL_HOME/* haoyu@node-0.${host}:/proj/bg-PG0/haoyu/scripts/
@@ -21,7 +25,8 @@ host="Nova.bg-PG0.apt.emulab.net"
 for ((i = 0; i < END; i++)); do
     current_node=${node_list[$i]}
     echo "building server on node $current_node"
-    # ssh -oStrictHostKeyChecking=no haoyu@node-$i.${host} "bash $setup_script/setup-ssh.sh"
+    echo "ssh -oStrictHostKeyChecking=no ruixuan@$current_node.${host} \"bash $setup_script/setup-ssh.sh\""
+    # ssh -oStrictHostKeyChecking=no ruixuan@$current_node.${host} "bash $setup_script/setup-ssh.sh"
 done
 
 echo ""
