93eddd368aac5f3f38c3631ec2b6c303e250f9d1
xzxz <46389288+J-XZ@users.noreply.github.com>
2024-04-15 19:03:25 +0800

    1

--- scripts/bootstrap/env/setup-apt-ssh.sh
+++ scripts/bootstrap/env/setup-apt-ssh.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
+# set -x
+
+user_name=ruixuan
+
 END=$1
-REMOTE_HOME="/proj/BG"
-REMOTE_HOME="/proj/bg-PG0"
-setup_script="$REMOTE_HOME/haoyu/scripts/env"
-limit_dir="$REMOTE_HOME/haoyu/scripts"
-LOCAL_HOME="/Users/haoyuh/Documents/PhdUSC/dblab/nova/NovaLSM/scripts/bootstrap"
+REMOTE_HOME="/users/${user_name}/NovaLSM"
+setup_script="$REMOTE_HOME/scripts/env"
+limit_dir="$REMOTE_HOME/scripts"
+LOCAL_HOME="/Users/jiangxuzhen/Downloads/e-lsmtree/NovaLSM/scripts/bootstrap"
 
-
-host="Nova.bg-PG0.apt.emulab.net"
+host="xz.dpmfs-pg0.clemson.cloudlab.us"
 # host="Nova.bg-PG0.utah.cloudlab.us"
-scp -r $LOCAL_HOME/* haoyu@node-0.${host}:/proj/bg-PG0/haoyu/scripts/
+scp -r $LOCAL_HOME/* ${user_name}@node0.${host}:/users/${user_name}/NovaLSM/scripts/
 
 for ((i=0;i<END;i++)); do
     echo "building server on node $i"
-    ssh -oStrictHostKeyChecking=no haoyu@node-$i.${host} "bash $setup_script/setup-ssh.sh"
+    ssh -oStrictHostKeyChecking=no ${user_name}@node$i.${host} "bash $setup_script/setup-ssh.sh"
 done
 
+echo ""
+
 for ((i=0;i<END;i++)); do
     echo "building server on node $i"
-    ssh -oStrictHostKeyChecking=no haoyu@node-$i.${host} "sudo cp $limit_dir/ulimit.conf /etc/systemd/user.conf"
-    ssh -oStrictHostKeyChecking=no haoyu@node-$i.${host} "sudo cp $limit_dir/sys_ulimit.conf /etc/systemd/system.conf"
-    ssh -oStrictHostKeyChecking=no haoyu@node-$i.${host} "sudo cp $limit_dir/limit.conf /etc/security/limits.conf"
-    ssh -oStrictHostKeyChecking=no haoyu@node-$i.${host} "sudo reboot"
+    ssh -oStrictHostKeyChecking=no ${user_name}@node$i.${host} "sudo cp $limit_dir/ulimit.conf /etc/systemd/user.conf"
+    ssh -oStrictHostKeyChecking=no ${user_name}@node$i.${host} "sudo cp $limit_dir/sys_ulimit.conf /etc/systemd/system.conf"
+    ssh -oStrictHostKeyChecking=no ${user_name}@node$i.${host} "sudo cp $limit_dir/limit.conf /etc/security/limits.conf"
+    ssh -oStrictHostKeyChecking=no ${user_name}@node$i.${host} "sudo reboot"
 done
