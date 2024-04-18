e2ba929fdc0431d542e859e6e1e23b5c30926e6d
xzxz <46389288+J-XZ@users.noreply.github.com>
2024-04-18 13:08:12 +0800

    1

--- scripts/bootstrap/env/setup-apt-ssh.sh
+++ scripts/bootstrap/env/setup-apt-ssh.sh
@@ -10,12 +10,14 @@ setup_script="$REMOTE_HOME/scripts/env"
 limit_dir="$REMOTE_HOME/scripts"
 LOCAL_HOME="/Users/jiangxuzhen/Downloads/e-lsmtree/NovaLSM/scripts/bootstrap"
 
-host="xz.dpmfs-pg0.clemson.cloudlab.us"
+host="xz.dpmfs-pg0.wisc.cloudlab.us"
 
 rm -r ./tmp_pubkeys/
-mkdir ./tmp_pubkeys
+mkdir -p ./tmp_pubkeys
 for ((i=0;i<END;i++)); do
     echo "building server on node $i"
+    echo "node: ${user_name}@node$i.${host}"
+    echo "rsync -avzP $LOCAL_HOME/* ${user_name}@node$i.${host}:${limit_dir}/"
     rsync -avzP $LOCAL_HOME/* ${user_name}@node$i.${host}:${limit_dir}/
     ssh -oStrictHostKeyChecking=no ${user_name}@node$i.${host} "bash $setup_script/setup-ssh.sh"
     # copy public key to local
