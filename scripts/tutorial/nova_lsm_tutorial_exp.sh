cec99b6a482aa36f262a35c9c49dc61b555a9f6e
xzxz <46389288+J-XZ@users.noreply.github.com>
2024-04-19 16:43:52 +0800

    tmp,change nova_lsm_tutorial_exp.sh

--- scripts/tutorial/nova_lsm_tutorial_exp.sh
+++ scripts/tutorial/nova_lsm_tutorial_exp.sh
@@ -10,7 +10,7 @@ recordcount="$1"
 dryrun="$2"
 
 script_dir="$home_dir/scripts"
-cache_bin_dir="$home_dir/nova"
+cache_bin_dir="$home_dir"
 client_bin_dir="/tmp/YCSB-Nova"
 results="/tmp/results"
 exp_results_dir="/users/ruixuan/nova-tutorial-$recordcount"
@@ -115,12 +115,11 @@ major_compaction_type="sc"
 major_compaction_max_parallism="32"
 major_compaction_max_tables_in_a_set="20"
 
-nmachines="7"
-
+nmachines="2"
 nservers="2"
-nclients="1"
+nclients="2"
 
-number_of_ltcs="3"
+number_of_ltcs="2"
 maxexecutiontime=1200
 dist="uniform"
 zipfianconstant="0.99"
@@ -136,10 +135,10 @@ function run_bench() {
 	n=0
 	while [ $n -lt $nservers ]; do
 		servers+=("node$i")
-		echo "node$i"
 		i=$((i + 1))
 		n=$((n + 1))
 	done
+	echo "servers: "${servers[@]}
 
 	i=0
 	n=0
@@ -153,15 +152,13 @@ function run_bench() {
 		i=$((i + 1))
 		n=$((n + 1))
 	done
+	echo "clients: "${clis[@]}
 
 	for ((i = 0; i < nmachines; i++)); do
 		id=$((i))
 		machines+=("node$id")
 	done
-
-	echo ${clis[@]}
-	echo ${servers[@]}
-	echo ${machines[@]}
+	echo "machines: "${machines[@]}
 
 	nova_servers=""
 	nova_all_servers=""
@@ -175,12 +172,22 @@ function run_bench() {
 		fi
 		nova_port=$((nova_port + 1))
 	done
+	echo "nova_all_servers: "${nova_all_servers[@]}
+	echo "nova_port: "$nova_port
+	# 每个server的port相同，nova_port是server的port + 1
+
 	nova_servers="${nova_servers:1}"
 	nova_all_servers="${nova_all_servers:1}"
 
+	echo "nova_servers: "$nova_servers
+	echo "nova_all_servers: "$nova_all_servers
+
 	current_time=$(date "+%Y-%m-%d-%H-%M-%S")
+	echo "current_time="$current_time
+
 	nstoc=$((nservers - number_of_ltcs))
-	echo "$nservers, $number_of_ltcs, $nstoc"
+	echo "nservers: $nservers, number_of_ltcs: $number_of_ltcs, nstoc: $nstoc"
+
 	result_dir_name="nova-zf-$zipfianconstant-nm-$num_memtables-lr-$num_log_replicas-try-$try-cfg-$change_cfg-d-$dist-w-$workload-ltc-$number_of_ltcs-stoc-$nstoc-l0-$l0_stop_write_mb-np-$num_memtable_partitions-nr-$cc_nranges_per_server"
 	echo "running experiment $result_dir_name"
 
@@ -198,48 +205,50 @@ function run_bench() {
 	ltc_config_path="$config_dir/nova-tutorial-config"
 
 	db_path="/db/nova-db-$recordcount-$value_size"
-	echo "$nova_servers $ltc_config_path $db_path"
+	echo "nova_servers:   $nova_servers"
+	echo "ltc_config_path:$ltc_config_path"
+	echo "db_path:        $db_path"
+
 	echo "cc servers $nova_all_servers"
 	if [[ $dryrun == "true" ]]; then
 		return
 	fi
 
 	for m in ${machines[@]}; do
-		echo "remove $results at machine $m"
-		ssh -oStrictHostKeyChecking=no $m "sudo rm -rf $results && mkdir -p $results && chmod -R 777 $results"
-		ssh -oStrictHostKeyChecking=no $m "sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'"
+		echo "remove and make new $results at machine $m, drop cache at machine $m"
+		su - ruixuan -c "ssh -oStrictHostKeyChecking=no $m \"sudo rm -rf $results && mkdir -p $results && chmod -R 777 $results\""
+		su - ruixuan -c "ssh -oStrictHostKeyChecking=no $m \"sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'\""
 	done
 
 	# restore the database image.
 	for s in ${servers[@]}; do
 		echo "restore database image $s"
-		ssh -oStrictHostKeyChecking=no $s "rm -rf /db/nova-db-$recordcount-1024/ && cp -r /db/snapshot-$cc_nranges_per_server-$nservers-$number_of_ltcs-$dist-$num_memtable_partitions-$memtable_size_mb-$zipfianconstant-$num_sstable_replicas/nova-db-$recordcount-1024/ /db/ &" &
+		echo "ssh -oStrictHostKeyChecking=no $s \"rm -rf /db/nova-db-$recordcount-1024/ && cp -r /db/snapshot-$cc_nranges_per_server-$nservers-$number_of_ltcs-$dist-$num_memtable_partitions-$memtable_size_mb-$zipfianconstant-$num_sstable_replicas/nova-db-$recordcount-1024/ /db/ &\" " "&"
+		su - ruixuan -c "ssh -oStrictHostKeyChecking=no $s \"rm -rf /db/nova-db-$recordcount-1024/ && cp -r /db/snapshot-$cc_nranges_per_server-$nservers-$number_of_ltcs-$dist-$num_memtable_partitions-$memtable_size_mb-$zipfianconstant-$num_sstable_replicas/nova-db-$recordcount-1024/ /db/ &\" " &
 	done
 
-	sleep 10
-
 	for m in ${machines[@]}; do
-		while ssh -oStrictHostKeyChecking=no $m "ps -ef | grep -v grep | grep -v ssh | grep -v bash | grep -c \"cp -r\""; do
-			sleep 10
-			echo "waiting for $m"
+		echo "waiting for $m"
+		while su - ruixuan -c "ssh -oStrictHostKeyChecking=no $m \"ps -ef | grep -v grep | grep -v ssh | grep -v bash | grep -c 'cp -r'\""; do
+			sleep 1
 		done
 	done
 
 	# start stats
-	echo "Preparing sar"
-	for m in ${machines[@]}; do
-		ssh -oStrictHostKeyChecking=no $m "sudo killall leveldb_main nova_shared_main nova_multi_thread_compaction nova_server_main java collectl sar"
-		ssh -oStrictHostKeyChecking=no $m "sudo collectl -scx -i 1 -P > $results/$m-coll.txt &"
-		ssh -oStrictHostKeyChecking=no $m "sar -P ALL 1 > $results/$m-cpu.txt &"
-		ssh -oStrictHostKeyChecking=no $m "sar -n DEV 1 > $results/$m-net.txt &"
-		ssh -oStrictHostKeyChecking=no $m "sar -r 1 > $results/$m-mem.txt &"
-		ssh -oStrictHostKeyChecking=no $m "sar -d 1 > $results/$m-disk.txt &"
-	done
+	# echo "Preparing sar"
+	# for m in ${machines[@]}; do
+	# 	su - ruixuan -c "ssh -oStrictHostKeyChecking=no $m \"sudo killall leveldb_main nova_shared_main nova_multi_thread_compaction nova_server_main java collectl sar\""
+	# 	su - ruixuan -c "ssh -oStrictHostKeyChecking=no $m \"sudo collectl -scx -i 1 -P > $results/$m-coll.txt &\""
+	# 	su - ruixuan -c "ssh -oStrictHostKeyChecking=no $m \"sar -P ALL 1 > $results/$m-cpu.txt &\""
+	# 	su - ruixuan -c "ssh -oStrictHostKeyChecking=no $m \"sar -n DEV 1 > $results/$m-net.txt &\""
+	# 	su - ruixuan -c "ssh -oStrictHostKeyChecking=no $m \"sar -r 1 > $results/$m-mem.txt &\""
+	# 	su - ruixuan -c "ssh -oStrictHostKeyChecking=no $m \"sar -d 1 > $results/$m-disk.txt &\""
+	# done
 
 	for m in ${machines[@]}; do
-		while ssh -oStrictHostKeyChecking=no $m "ps -ef | grep -v grep | grep -v ssh | grep -v bash | grep -c nova_server_main"; do
-			sleep 10
-			echo "waiting for $m"
+		echo "waiting for $m"
+		while su - ruixuan -c "ssh -oStrictHostKeyChecking=no $m \"ps -ef | grep -v grep | grep -v ssh | grep -v bash | grep -c nova_server_main\""; do
+			sleep 1
 		done
 	done
 
@@ -247,21 +256,23 @@ function run_bench() {
 	for s in ${servers[@]}; do
 		echo "creating server on $s"
 		nova_rdma_port=$((rdma_port))
+		echo "nova_rdma_port: $nova_rdma_port"
 		cmd="stdbuf --output=0 --error=0 ./nova_server_main --ltc_migration_policy=$ltc_migration_policy --enable_range_index=$enable_range_index --num_migration_threads=$num_migration_threads --num_sstable_replicas=$num_sstable_replicas --level=$level --l0_start_compaction_mb=$l0_start_compaction_mb --subrange_no_flush_num_keys=$subrange_no_flush_num_keys --enable_detailed_db_stats=$enable_detailed_db_stats --major_compaction_type=$major_compaction_type --major_compaction_max_parallism=$major_compaction_max_parallism --major_compaction_max_tables_in_a_set=$major_compaction_max_tables_in_a_set --enable_flush_multiple_memtables=$enable_flush_multiple_memtables --recover_dbs=$recover_dbs --num_recovery_threads=$num_recovery_threads  --sampling_ratio=1 --zipfian_dist_ref_counts=$zipfian_dist_file_path --client_access_pattern=$dist  --memtable_type=static_partition --enable_subrange=$enable_subrange --num_log_replicas=$num_log_replicas --log_record_mode=$log_record_mode --scatter_policy=$scatter_policy --number_of_ltcs=$number_of_ltcs --enable_lookup_index=$enable_lookup_index --l0_stop_write_mb=$l0_stop_write_mb --num_memtable_partitions=$num_memtable_partitions --num_memtables=$num_memtables --num_rdma_bg_workers=$num_rdma_bg_workers --db_path=$db_path --num_storage_workers=$num_storage_workers --stoc_files_path=$cc_stoc_files_path --max_stoc_file_size_mb=$max_stoc_file_size_mb --sstable_size_mb=$sstable_size_mb --ltc_num_stocs_scatter_data_blocks=$ltc_num_stocs_scatter_data_blocks --all_servers=$nova_servers --server_id=$server_id --mem_pool_size_gb=$mem_pool_size_gb --use_fixed_value_size=$value_size --ltc_config_path=$ltc_config_path --ltc_num_client_workers=$cc_nconn_workers --num_rdma_fg_workers=$num_rdma_fg_workers --num_compaction_workers=$num_compaction_workers --block_cache_mb=$block_cache_mb --row_cache_mb=$row_cache_mb --memtable_size_mb=$memtable_size_mb --cc_log_buf_size=$cc_log_buf_size --rdma_port=$rdma_port --rdma_max_msg_size=$rdma_max_msg_size --rdma_max_num_sends=$rdma_max_num_sends --rdma_doorbell_batch_size=$rdma_doorbell_batch_size --enable_rdma=$enable_rdma --enable_load_data=$enable_load_data --use_local_disk=$use_local_disk"
 		echo "$cmd"
-		ssh -oStrictHostKeyChecking=no $s "mkdir -p $cc_stoc_files_path && mkdir -p $db_path && cd $cache_bin_dir && $cmd >& $results/server-$s-out &" &
+		su - ruixuan -c "ssh -oStrictHostKeyChecking=no $s \"mkdir -p $cc_stoc_files_path && mkdir -p $db_path && cd $cache_bin_dir && $cmd >& $results/server-$s-out &\"" &
 		server_id=$((server_id + 1))
 		nova_rdma_port=$((nova_rdma_port + 1))
 		sleep 1
 	done
 
 	sleep 30
+
 	for c in ${clis[@]}; do
 		for i in $(seq 1 $nclients_per_server); do
 			echo "creating client on $c-$i"
 			cmd="stdbuf --output=0 --error=0 bash $script_dir/run_ycsb.sh $nthreads $nova_all_servers $debug $partition $recordcount $maxexecutiontime $dist $value_size $workload $ltc_config_path $cardinality $operationcount $zipfianconstant 0"
 			echo "$cmd"
-			ssh -oStrictHostKeyChecking=no $c "cd $client_bin_dir && $cmd >& $results/client-$c-$i-out &" &
+			su - ruixuan -c "ssh -oStrictHostKeyChecking=no $c \"cd $client_bin_dir && $cmd >& $results/client-$c-$i-out &\"" &
 		done
 	done
 
@@ -276,11 +287,10 @@ function run_bench() {
 	stop="false"
 	max_wait_time=$((maxexecutiontime + 1200))
 	for m in ${clis[@]}; do
-		while ssh -oStrictHostKeyChecking=no $m "ps -ef | grep -v \"grep --color=auto ycsb\" | grep -v ssh | grep -v bash | grep ycsb | grep -c java"; do
-			sleep 10
+		echo "waiting for $m for $sleep_time seconds"
+		while su - ruixuan -c "ssh -oStrictHostKeyChecking=no $m \"ps -ef | grep -v 'grep --color=auto ycsb' | grep -v ssh | grep -v bash | grep ycsb | grep -c java\""; do
+			sleep 1
 			sleep_time=$((sleep_time + 10))
-			echo "waiting for $m for $sleep_time seconds"
-
 			if [[ $sleep_time -gt $max_wait_time ]]; then
 				stop="true"
 				break
@@ -296,18 +306,20 @@ function run_bench() {
 	for s in ${servers[@]}; do
 		cmd="du -sm $db_path"
 		echo "$cmd"
-		ssh -oStrictHostKeyChecking=no $s "$cmd >& $results/server-$s-db-disk-space"
+		su - ruixuan -c "ssh -oStrictHostKeyChecking=no $s \"$cmd >& $results/server-$s-db-disk-space\""
+		su - ruixuan -c "ssh -oStrictHostKeyChecking=no $s \"$cmd\""
 	done
 
 	for s in ${servers[@]}; do
 		cmd="du -sm $cc_stoc_files_path"
 		echo "$cmd"
-		ssh -oStrictHostKeyChecking=no $s "$cmd >& $results/server-$s-rtable-disk-space"
+		su - ruixuan -c "ssh -oStrictHostKeyChecking=no $s \"$cmd >& $results/server-$s-rtable-disk-space\""
 	done
 
 	for m in ${machines[@]}; do
 		echo "kill java at $m"
-		ssh -oStrictHostKeyChecking=no $m "sudo killall leveldb_main nova_shared_main nova_multi_thread_compaction nova_server_main java collectl sar"
+		su - ruixuan -c "ssh -oStrictHostKeyChecking=no $m \"sudo killall leveldb_main nova_shared_main nova_multi_thread_compaction nova_server_main java collectl sar\""
+		echo ""
 	done
 
 	dir="$exp_results_dir/$result_dir_name"
@@ -319,17 +331,17 @@ function run_bench() {
 	# DB logs.
 	server_id=0
 	for s in ${servers[@]}; do
-		ssh -oStrictHostKeyChecking=no $s "mkdir -p $results/server-$server_id-dblogs/ && cp -r $db_path/*/LOG* $results/server-$server_id-dblogs/"
+		su - ruixuan -c "ssh -oStrictHostKeyChecking=no $s \"mkdir -p $results/server-$server_id-dblogs/ && cp -r $db_path/*/LOG* $results/server-$server_id-dblogs/\""
 		# ssh -oStrictHostKeyChecking=no $s "rm -rf $db_path && rm -rf $cc_stoc_files_path"
 		server_id=$((server_id + 1))
 	done
 
 	for m in ${machines[@]}; do
-		scp -r $m:$results/* $dir
+		su - ruixuan -c "scp -r $m:$results/* $dir"
 	done
 	sleep 10
 }
 
 run_bench
 
-python /proj/bg-PG0/haoyu/scripts/parse_ycsb_nova_leveldb.py $nmachines $exp_results_dir >stats_tutorial_out
+python2 /users/ruixuan/NovaLSM/scripts/exp/parse_ycsb_nova_leveldb.py $nmachines $exp_results_dir >stats_tutorial_out
