a637061b68e02d8eb8715a9507ae05374238d436
Xuzhen Jiang <jiangxuzhen@hhu.edu.cn>
2024-05-03 10:30:34 +0800

    Update setup-apt-ssh.sh

--- scripts/bootstrap/env/setup-apt-ssh.sh
+++ scripts/bootstrap/env/setup-apt-ssh.sh
@@ -15,10 +15,10 @@ sudo mkdir -p ./tmp_pubkeys
 for ((i=0;i<END;i++)); do
     echo "0 building server on node $i"
     echo "node: ${user_name}@node$i"
-    sudo rsync -avzP $LOCAL_HOME/* ${user_name}@node$i:${limit_dir}/ -e /Users/jiangxuzhen/.ssh/config
+    sudo rsync -avzP $LOCAL_HOME/* ${user_name}@node$i:${limit_dir}/ -e "ssh -F /Users/jiangxuzhen/.ssh/config"
     sudo ssh -oStrictHostKeyChecking=no ${user_name}@node$i "bash $setup_script/setup-ssh.sh" -F /Users/jiangxuzhen/.ssh/config
     # copy public key to local
-    sudo rsync -avzP ${user_name}@node$i:~/.ssh/id_rsa.pub ./tmp_pubkeys/node$i.pub -e /Users/jiangxuzhen/.ssh/config
+    sudo rsync -avzP ${user_name}@node$i:~/.ssh/id_rsa.pub ./tmp_pubkeys/node$i.pub -e "ssh -F /Users/jiangxuzhen/.ssh/config"
 done
 
 echo ""
